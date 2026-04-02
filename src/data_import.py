d16998019b7d01f516c0ead98f28b1b120c1ee7c
Sara <gunawardhanapulsara@gmail.com>
2026-04-02 16:19:24 +0100

    data importing

--- src/data_import.py
+++ src/data_import.py
@@ -1,1 +1,17 @@
 import requests 
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env into os.environ
+load_dotenv()
+
+# Retrieve the key
+api_key = os.getenv("MY_API_KEY")
+
+if not api_key:
+    raise ValueError("API Key not found!")
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey='+api_key
+r = requests.get(url)
+data = r.json()
+
+print(data)
