eaf8555ef3fca2225e068336f1b3c092819b4236
Sara <gunawardhanapulsara@gmail.com>
2026-04-07 22:47:32 +0100

    trying to add the data in

--- src/data_import.py
+++ src/data_import.py
@@ -4,34 +4,35 @@ import os
 import pandas as pd
 
 # Load variables from .env into os.environ
-load_dotenv()
+def importing_to_df(): 
+    load_dotenv()
 
-# Retrieve the key
-api_key = os.getenv("MY_API_KEY")
+    # Retrieve the key
+    api_key = os.getenv("MY_API_KEY")
 
-if not api_key:
-    raise ValueError("API Key not found!")
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey='+api_key
-r = requests.get(url)
-data = r.json()
-print(data.keys())
-df_meta = pd.DataFrame(data['Meta Data'], index=[0])
-df = pd.DataFrame(data['Time Series (Daily)']).T.reset_index()
-df = df.rename({
-    'index' : 'date',
-    '1. open':'open',
-    '2. high':'high',
-    '3. low':'low',
-    '4. close':'close',
-    '5. volume':'volume',
-},axis=1)
-df['company'] = pd.DataFrame(data['Meta Data'],index=[0])['2. Symbol'][0]
-df = df.astype({
-    'open':'float',
-    'high':'float',
-    'low':'float',
-    'close':'float',
-    'volume':'float'
-})
-df['date']= pd.to_datetime(df['date'])
-print(df.info())
+    if not api_key:
+        raise ValueError("API Key not found!")
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey='+api_key
+    r = requests.get(url)
+    data = r.json()
+    print(data.keys())
+    df_meta = pd.DataFrame(data['Meta Data'], index=[0])
+    df = pd.DataFrame(data['Time Series (Daily)']).T.reset_index()
+    df = df.rename({
+        'index' : 'date',
+        '1. open':'open',
+        '2. high':'high',
+        '3. low':'low',
+        '4. close':'close',
+        '5. volume':'volume',
+    },axis=1)
+    df['company'] = pd.DataFrame(data['Meta Data'],index=[0])['2. Symbol'][0]
+    df = df.astype({
+        'open':'float',
+        'high':'float',
+        'low':'float',
+        'close':'float',
+        'volume':'float'
+    })
+    df['date']= pd.to_datetime(df['date'])
+    return df
--- src/etl_load.py
+++ src/etl_load.py
@@ -1,32 +1,43 @@
-from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP
+from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, REAL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 from connection import set_up_connection 
 import psycopg2
+from data_import import importing_to_df
+
+df = importing_to_df()
 
 Base = declarative_base()
 
 class stock(Base): 
-    __tablenamne__ = 'stocks'
+    __tablename__ = 'stocks'
+    id = Column(Integer, primary_key=True, autoincrement=True)
     date = Column(TIMESTAMP)
-    open = Column(Integer) 
-    high = Column(Integer) 
-    low = Column(Integer) 
-    close = Column(Integer) 
+    open = Column(REAL) 
+    high = Column(REAL) 
+    low = Column(REAL) 
+    close = Column(REAL) 
     volume = Column(Integer)
     
 #Setting up the connection 
 engine = create_engine('postgresql://postgres:admin@localhost:5432/stocks', echo=True)
-with engine.connect() as conn:
-    # session instance is used to communicate with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
             
 
-
-
+for _, row in df.iterrows():
+    stock_record = stock(
+         date=row['date'],
+         open=row['open'],
+         high=row['high'],
+         low=row['low'],
+         close=row['close'],
+         volume=row['volume']
+     )
+    session.add(stock_record)
+session.commit()
 
     
     
