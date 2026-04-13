bc2f3053393337b4f44682b4302be7610787484b
Sara <gunawardhanapulsara@gmail.com>
2026-04-13 21:55:57 +0100

    data auditing

--- src/data_audit.py
+++ src/data_audit.py
@@ -1,3 +1,7 @@
 from data_import import importing_to_df
-
+from datetime import datetime 
+dt = datetime.now()
 df = importing_to_df()
+print(df.info())
+df_test = df[df['date'] > '2026-04-07']
+print(df_test)
